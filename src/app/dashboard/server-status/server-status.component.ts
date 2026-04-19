973ace222e89f9d87758c04db82274e59dfe1597
faqfaraan <faqfaraan@gmail.com>
2026-04-19 19:27:08 -0400

    implementing lifecycle interfaces

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,26 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit{
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {
     setInterval(() => {
       const rnd = Math.random();
 
       if (rnd < 0.5) {
         this.currentStatus = 'online';
-      } else if ( rnd < 0.9) {
+      } else if (rnd < 0.9) {
         this.currentStatus = 'offline';
       } else {
         this.currentStatus = 'unknown';
       }
-    }, 3000)
+    }, 3000);
   }
 }
